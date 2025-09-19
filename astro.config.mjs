85f4d4849bae359979350219e70ea39c434379b5
CelsoBlackfyre <celsomartinscarvalho@gmail.com>
2025-09-19 09:21:17 -0300

    updt8

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
-// @ts-check
+﻿// @ts-check
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.SITE_URL ?? 'https://blackfyre-codex.xyz',
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: 'static',
   integrations: [mdx(), sitemap()],
 })
